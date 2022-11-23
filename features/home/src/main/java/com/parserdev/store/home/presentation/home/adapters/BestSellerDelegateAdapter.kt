a768bd64b2acd8b4999ca2ebce848a6e7998b0b7
ParSerDev <paravyansergey@gmail.com>
2022-11-24 02:39:35 +0300

    feat: add BestSellerDelegateAdapter.kt, add BestSellersListItem.kt

---
+++ features/home/src/main/java/com/parserdev/store/home/presentation/home/adapters/BestSellerDelegateAdapter.kt
@@ -0,0 +1,57 @@
+package com.parserdev.store.home.presentation.home.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.parserdev.store.data.dto.home.FavouriteItemDto
+import com.parserdev.store.home.databinding.ItemDelegateBestSellerBinding
+import com.parserdev.store.home.presentation.home.adapters.delegate.DelegateAdapter
+import com.parserdev.store.home.presentation.home.adapters.delegate.DelegateAdapterItem
+import com.parserdev.store.home.presentation.home.adapters.model.BestSellersListItem
+
+class BestSellerDelegateAdapter(
+    private val likeClickListener: (FavouriteItemDto) -> Unit,
+    private val navigationClickListener: (String) -> Unit,
+    private val marginLeft: Int,
+    private val marginRight: Int
+) :
+    DelegateAdapter<BestSellersListItem, BestSellerDelegateAdapter.BestSellerDelegateViewHolder>(
+        BestSellersListItem::class.java
+    ) {
+    override fun createViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
+        val binding =
+            ItemDelegateBestSellerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return BestSellerDelegateViewHolder(
+            binding = binding,
+            likeClickListener = likeClickListener,
+            navigationClickListener = navigationClickListener,
+            marginLeft = marginLeft,
+            marginRight = marginRight
+        )
+    }
+
+    override fun bindViewHolder(
+        model: BestSellersListItem,
+        viewHolder: BestSellerDelegateViewHolder,
+        payloads: List<DelegateAdapterItem.Payloadable>
+    ) {
+        viewHolder.bind(model)
+    }
+
+    class BestSellerDelegateViewHolder(
+        private val binding: ItemDelegateBestSellerBinding,
+        private val likeClickListener: (FavouriteItemDto) -> Unit,
+        private val navigationClickListener: (String) -> Unit,
+        private val marginLeft: Int,
+        private val marginRight: Int
+    ) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: BestSellersListItem) {
+            binding.apply {
+
+            }
+        }
+
+    }
+}
